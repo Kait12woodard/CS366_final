b2561f1c9015bb9e54e8b45462e88cb9c702373f
Kaitlyn Woodard <kwoodard22@mail.wou.edu>
2024-03-08 14:25:01 -0800

    Parameter bounded form inputs. Added health status if-statement and passed to next page successfully.

--- FinalSolution/FinalProject/Controllers/BuddyController.cs
+++ FinalSolution/FinalProject/Controllers/BuddyController.cs
@@ -4,9 +4,91 @@ namespace FinalProject.Controllers;
 
 public class BuddyController : Controller 
 {
+
     [HttpGet]
-    public IActionResult BuddyForm()
+    public IActionResult BuddyForm(string? pokemon, double? weight, string? allergy, string? food, string? nickname)
     {
-        return View();
+        if(pokemon != null || weight != null || allergy != null || food != null || nickname != null)
+        {
+            List<string> summary = new List<string>();
+            summary.Add(pokemon);
+            string stringWeight = weight.ToString();
+            summary.Add(stringWeight);
+            if(pokemon == "Mimikyu")
+            {
+                if(weight > 4)
+                {
+                    summary.Add("Overweight");
+                } else if(weight < 1)
+                {
+                    summary.Add("Underweight");
+                } else 
+                {
+                    summary.Add("Regular");
+                }
+            }
+            else if(pokemon == "Eevee")
+            {
+                if(weight > 20)
+                {
+                    summary.Add("Overweight");
+                } else if(weight < 8)
+                {
+                    summary.Add("Underweight");
+                }
+                else
+                {
+                    summary.Add("Regular");
+                }
+            }
+            else if(pokemon == "Gengar")
+            {
+                if(weight > 110)
+                {
+                    summary.Add("Overweight");
+                } else if(weight < 70)
+                {
+                    summary.Add("Underweight");
+                }
+                else
+                {
+                    summary.Add("Regular");
+                }
+            }
+            else if(pokemon == "Charizard")
+            {
+                if(weight > 230)
+                {
+                    summary.Add("Overweight");
+                } else if(weight < 170)
+                {
+                    summary.Add("Underweight");
+                }
+                else
+                {
+                    summary.Add("Regular");
+                }
+            }
+            else
+            {
+                if(weight > 550)
+                {
+                    summary.Add("Overweight");
+                } else if(weight < 420)
+                {
+                    summary.Add("Underweight");
+                }
+                else
+                {
+                    summary.Add("Regular");
+                }
+            }
+            summary.Add(allergy);
+            summary.Add(food);
+            summary.Add(nickname);
+
+            return View("Summary", summary);
+        }
+    return View();
     }
 }
--- FinalSolution/FinalProject/wwwroot/js/site.js
+++ FinalSolution/FinalProject/wwwroot/js/site.js
@@ -3,3 +3,12 @@
         $(this).attr("src", "/Images/SnorlaxWave.png");
     })
 });
+
+document.getElementById("Convert").addEventListener("click", function() {
+    const stringWeight = document.getElementById("Weight_to_convert");
+    const blsWeight = parseFloat(stringWeight.innerText);
+    const kiloWeight = blsWeight * 0.453592;
+    stringWeight.innerText = kiloWeight;
+    const label = document.getElementById("WeightLabel");
+    label.innerText = "kg"
+});
