27190a3f6c712f2596d438bd7ea98e4afe7dbdf5
Kaitlyn Woodard <kwoodard22@mail.wou.edu>
2024-03-10 12:43:02 -0700

    Created and added photos for summary page. Implemented function to display image dependant on pokemon and health status.

--- FinalSolution/FinalProject/wwwroot/js/site.js
+++ FinalSolution/FinalProject/wwwroot/js/site.js
@@ -1,9 +1,11 @@
-﻿$(document).ready(function(){
+﻿//Home page Snorlax Animation
+$(document).ready(function(){
     $("#snorlax").click(function(){
         $(this).attr("src", "/Images/SnorlaxWave.png");
     })
 });
 
+//Converts weight from lbs to kg
 $("#Convert_lbs").hide();
 
 document.getElementById("Convert_kilo").addEventListener("click", function() {
@@ -17,6 +19,7 @@ document.getElementById("Convert_kilo").addEventListener("click", function() {
     $("#Convert_lbs").show();
 });
 
+//Converts weight from kg to lbs
 document.getElementById("Convert_lbs").addEventListener("click", function() {
     const stringWeight = document.getElementById("Weight_to_convert");
     const blsWeight = parseFloat(stringWeight.innerText);
@@ -28,65 +31,139 @@ document.getElementById("Convert_lbs").addEventListener("click", function() {
     $("#Convert_kilo").show();
 });
 
+//Based on Pokemon displays recommended food and foods to avoid
 $(document).ready(function() {
     const pokemon = $("#PokemonType").text();
-    var count = 0;
-    var food = [];
+    var recommendCount = 0;
+    var avoidCount = 0;
+    var recommendFood = [];
+    var avoidFood = [];
 
     if(pokemon === "Mimikyu")
     {
-        food = ["Basil", "Jalapenos", "Onions"]
+        recommendFood = ["Basil", "Jalapenos", "Onions"];
+        avoidFood = ["Apples", "Hamburgers", "Fried Fillet"];
     } else if(pokemon === "Eevee")
     {
-        food = ["Bananas", "Fried Fillet", "Tofu"]
+        recommendFood = ["Bananas", "Fried Fillet", "Tofu"];
+        avoidFood = ["Pickles", "Strawberrys", "Herbed Sausage"];
     } else if(pokemon === "Gengar")
     {
-        food = ["Ketchup", "Kiwis", "Green Bell Peppers"]
+        recommendFood = ["Ketchup", "Kiwis", "Green Bell Peppers"];
+        avoidFood = ["Fried Fillet", "Strawberrys", "Pinapples"];
     } else if(pokemon === "Charizard")
     {
-        food = ["Peanut Butter", "Apples", "Kiwis"]
+        recommendFood = ["Peanut Butter", "Apples", "Kiwis"];
+        avoidFood = ["Jalapenos", "Mustard", "Chili Sauce"];
     } else if(pokemon === "Lapras")
     {
-        food = ["Cucumbers", "Apples", "Herbed Sausage"]
+        recommendFood = ["Cucumbers", "Apples", "Herbed Sausage"];
+        avoidFood = ["Horseradishs", "Bacon", "Bananas"];
     }
     $("#RecommendButton").click(function(){
-        $("#recFood").append(food[count]+ "<br>");
-        count++;
-        if(count === 3) 
+        $("#recFood").append(recommendFood[recommendCount]+ "<br>");
+        recommendCount++;
+        if(recommendCount === 3) 
         {
             $("#RecommendButton").hide();
         }
     });  
-});
 
-$(document).ready(function() {
-    const pokemon = $("#PokemonType").text();
-    var count = 0;
-    var food = [];
-
-    if(pokemon === "Mimikyu")
-    {
-        food = ["Apples", "Hamburgers", "Fried Fillet"]
-    } else if(pokemon === "Eevee")
-    {
-        food = ["Pickles", "Strawberrys", "Herbed Sausage"]
-    } else if(pokemon === "Gengar")
-    {
-        food = ["Fried Fillet", "Strawberrys", "Pinapples"]
-    } else if(pokemon === "Charizard")
-    {
-        food = ["Jalapenos", "Mustard", "Chili Sauce"]
-    } else if(pokemon === "Lapras")
-    {
-        food = ["Horseradishs", "Bacon", "Bananas"]
-    }
     $("#AvoidButton").click(function(){
-        $("#avoidFood").append(food[count]+ "<br>");
-        count++;
-        if(count === 3) 
+        $("#avoidFood").append(avoidFood[avoidCount]+ "<br>");
+        avoidCount++;
+        if(avoidCount === 3) 
         {
             $("#AvoidButton").hide();
         }
-    });  
+    }); 
+});
+
+//Displays image based on pokemon and weight
+$(document).ready(function() {
+    const pokemon = $("#PokemonType").text();
+    var weightStatus = $("#Health").text();
+    weightStatus = weightStatus.slice(15);
+    var image = "";
+    var alt = "";
+
+    if(pokemon === "Mimikyu")
+    {
+        if(weightStatus === "Overweight")
+        {
+            image = "MimikyuChunky.png";
+            alt = "Chunky Mimikyu";
+        } else if(weightStatus === "Underweight")
+        {
+            image = "MimikyuThin.png";
+            alt = "Thin Mimikyu";
+        } else
+        {
+            image = "MimikyuReg.png";
+            alt = "Regular Mimikyu";
+        }
+    } else if(pokemon === "Eevee")
+    {
+        if(weightStatus === "Overweight")
+        {
+            image = "EeveeChunky.png";
+            alt = "Chunky Eevee";
+        } else if(weightStatus === "Underweight")
+        {
+            image = "EeveeThin.png";
+            alt = "Eevee Thin";
+        } else
+        {
+            image = "EeveeReg.png";
+            alt = "Regular Eevee";
+        }
+    } else if(pokemon === "Gengar")
+    {
+        if(weightStatus === "Overweight")
+        {
+            image = "GengarChunky.png";
+            alt = "Chunky Gengar";
+        } else if(weightStatus === "Underweight")
+        {
+            image = "GengarThin.png";
+            alt = "Thin Gengar";
+        } else
+        {
+            image = "GengarReg.png";
+            alt = "Regular Gengar";
+        }
+    } else if(pokemon === "Charizard")
+    {
+        if(weightStatus === "Overweight")
+        {
+            image = "CharizardChunky.png";
+            alt = "Chunky Charizard";
+        } else if(weightStatus === "Underweight")
+        {
+            image = "CharizardThin.png";
+            alt = "Thin Charizard";
+        } else
+        {
+            image = "CharizardReg.png";
+            alt = "Regular Charizard";
+        }
+    } else if(pokemon === "Lapras")
+    {
+        if(weightStatus === "Overweight")
+        {
+            image = "LaprasChunky.png";
+            alt = "Chunky Lapras";
+        } else if(weightStatus === "Underweight")
+        {
+            image = "LaprasThin.png";
+            alt = "Thin Lapras";
+        } else
+        {
+            image = "LaprasReg.png";
+            alt = "Regular Lapras";
+        }
+    }
+
+    $("#PokemonImage").append("<img src= '/Images/"+ image + "' alt= '" + alt +"'>");
 });
 
